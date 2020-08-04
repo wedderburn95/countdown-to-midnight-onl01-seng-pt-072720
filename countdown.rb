860ad917d6b26ee992143947b695054be92feaa8
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-04 00:54:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 10
+  puts "#{seconds_to_midnight} SECOND(S)!"
+  seconds_to_midnight -= 1
+end
+  "HAPPY NEW YEAR!"
+end
 
-
-def countdown
+def  countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+  puts "#{seconds_to_midnight} SECOND(S)"
+  sleep(1)
+  seconds_to_midnight -= 1
+end
+  "HAPPY NEW YEAR!"
 end
